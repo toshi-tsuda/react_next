cb6b01ded056876c4bde3f60b77e24a456eeea4b
toshihiko_tsuda <tsuda@gio-cubic.co.jp>
2024-09-20 14:34:31 +0900

    first commit

---
+++ app/sample/sample.js
@@ -0,0 +1,11 @@
+
+
+export default function Sample() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>test</h1>
+      </div>
+    </main>
+  );
+}
